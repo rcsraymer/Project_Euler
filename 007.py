41a22e899bc871369f797882ffc131163dbda122
rcsraymer <rcs.raymer@gmail.com>
2020-02-13 10:31:13 -0500

    Update 007.py

--- 007.py
+++ 007.py
@@ -1,44 +1,56 @@
 # By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13, we can see that the 6th prime is 13.
 # What is the 10 001st prime number?
+
+# Answer is 104743
+
 import time
 import math
-
 
 start = time.time()
 
 # Determine number of primes
-prime_count = 1000
+prime_count = 10001
 
-chk_lst = []
+# Empty list to store primes
+prime_lst = []
 
+# Initialize increment number to first natural prime
 inc_num = 2
 
-x = lambda m: (m == 2 or m % 2 != 0) and (m == 3 or m % 3 != 0)
-#
-#while len(chk_lst) < prime_count:
-#    for i in chk_lst:
-#        z = lambda m: (m == i or m % i != 0)
-#        filtered_list = [num for num in list(filter(x, filtered_list)) if n % num == 0]
-#       
-#
-#    chk_lst.append(inc_num)
-#    chk_lst = list(filter(x,chk_lst))
-#    inc_num = inc_num + 1
+# If length of prime_lst is less than prime_count, go
+while len(prime_lst) < prime_count:
+    
+    # If iteration_count > 0, current inc_num not added to prime_lst
+    iteration_count = 0
+    
+    # We only test whether num is divisible up to the square root of the num
+    s = int(math.sqrt(inc_num))
+    
+    # Create list with range of 2 -> s + 1
+    chk_lst = list(range(2, s+1))
+    
+    # For each num in chk_lst, see if inc_num % num in list does not have remainder
+    # If no remainder: pass else add 1 to iteration_count and pass loop
+    for i in chk_lst:
+        if iteration_count == 0:
+            if (inc_num % i != 0):
+                pass
+            else:
+                iteration_count =+ 1
+        else:
+            pass
+    
+    # Only add inc_num to prime_lst if iteration_count == 0
+    if iteration_count == 0:
+        prime_lst.append(inc_num)
+    else:
+        pass
+    
+    # Increment inc_num by 1
+    inc_num = inc_num + 1
 
-print(chk_lst)
+print(prime_lst)
 end = time.time()
 print(end - start)
 
-
-prime_count = 1000
-chk_lst = list(range(2, prime_count+1))
-print(chk_lst)
-
-n = 1000
-
-s = int(math.sqrt(n))
-
-print(s)
-
-# Without sq root = 0.5504837036132812
-# With sq root chck = 
+# 1.3029999733 second execution
